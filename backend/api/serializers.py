7cbf0095d6afd14683d6ca86db495827b3947492
Danzan Tankhaev <dtankhaev@gmail.com>
2023-04-20 18:22:48 -0400

    fix

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,7 +1,5 @@
-from django.shortcuts import get_object_or_404
 from drf_extra_fields.fields import Base64ImageField
 from rest_framework import serializers
-from rest_framework.exceptions import ValidationError
 
 from recipes.models import Ingredient, IngredientAmount, Recipe, Tag
 from users.serializers import CustomUserSerializer
