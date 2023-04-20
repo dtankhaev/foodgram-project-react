38e331b71b267f46da08ab5a450ec52509b17dbc
Danzan Tankhaev <dtankhaev@gmail.com>
2023-04-20 18:15:36 -0400

    fix

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,5 +1,7 @@
+from django.shortcuts import get_object_or_404
 from drf_extra_fields.fields import Base64ImageField
 from rest_framework import serializers
+from rest_framework.exceptions import ValidationError
 
 from recipes.models import Ingredient, IngredientAmount, Recipe, Tag
 from users.serializers import CustomUserSerializer
@@ -63,6 +65,21 @@ class WriteRecipeSerializer(serializers.ModelSerializer):
         self.create_ingredients(instance, ingredients)
         instance.save
         return instance
+
+    def validate_ingredients(self, ingredients):
+        if not ingredients:
+            raise ValidationError({'Нужен хотя бы один ингредиент!'})
+        ingredients_list = []
+        for obj in ingredients:
+            ingredient = get_object_or_404(Ingredient, id=obj['id'])
+            if ingredient in ingredients_list:
+                raise ValidationError({'Ингредиенты не должны повторяться!'})
+            if int(obj['amount']) <= 0:
+                raise ValidationError(
+                    {'Количество ингредиента должно быть больше 0!'}
+                                     )
+            ingredients_list.append(ingredient)
+        return ingredients
 
     def to_representation(self, instance):
         return ReadRecipeSerializer(
