1b3613ccf8f6a595b76d09e0b3a1355d67b46660
Danzan Tankhaev <dtankhaev@gmail.com>
2023-04-20 03:54:10 -0400

    test6

--- backend/api/permissions.py
+++ backend/api/permissions.py
@@ -14,5 +14,5 @@ class AuthorOrReadOnly(BasePermission):
                 or request.user.is_authenticated)
 
     def has_object_permission(self, request, view, obj):
-        return (request.method in SAFE_METHODS or
-                obj.author == request.user)
+        return (request.method in SAFE_METHODS
+                or obj.author == request.user)
